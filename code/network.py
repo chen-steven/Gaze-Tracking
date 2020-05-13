498d6b5192c79c859cb2c53337435633e498309b
Steven Chen <syc.stevenchen@gmail.com>
2020-05-13 16:24:00 -0400

    fix

--- code/network.py
+++ code/network.py
@@ -5,12 +5,20 @@ import torch
 class GazeEstimationNet(nn.Module):
     def __init__(self):
         super(GazeEstimationNet, self).__init__()
-        self.conv1 = nn.Conv2d(3,96, kernel_size=11, stride=4)
-        self.skip_conv1 = nn.Conv2d(96, 256, kernel_size=7, stride=4)
-        self.features1 = nn.Sequential(
+        
+      
+
+        self.features0 = nn.Seqeuntial(
+            nn.Conv2d(3,96, kernel_size=11, stride=4)
             nn.BatchNorm2d(96),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.ReLU(True),
+        )
+
+        self.skip_conv1 = nn.Conv2d(96, 256, kernel_size=7, stride=4)
+
+        self.features1 = nn.Sequential(
+           
             nn.Conv2d(96, 256, kernel_size=5, stride=2),
             nn.BatchNorm2d(256),
             nn.MaxPool2d(kernel_size=3, stride=2),
@@ -41,25 +49,24 @@ class GazeEstimationNet(nn.Module):
 
 
     def forward(self,x):
-        x = F.relu(self.conv1(x))
+        x = self.features0(x)
         x1 = self.skip_conv1(x)
-
         x = self.features1(x)
 
-        x2 = F.relu(self.skip_conv2(x))
+        # x2 = F.relu(self.skip_conv2(x))
 
-        x3 = torch.cat((x1,x2),1)
-        x3 = self.skip_conv3(x3)
+        # x3 = torch.cat((x1,x2),1)
+        # x3 = self.skip_conv3(x3)
 
-        x = self.features2(x)
-        x = torch.cat((x,x3),1)
+        # x = self.features2(x)
+        # x = torch.cat((x,x3),1)
 
-        x = torch.flatten(x,1)
+        # x = torch.flatten(x,1)
 
         #x = self.regression(x)
         return x
 
 if __name__ == '__main__':
-    #from torchsummary import summary
+    from torchsummary import summary
     model = GazeEstimationNet()
-    print(model)
+    print(summary(model, (3,227,227)))
