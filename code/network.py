c261588c23bcccf39fd6e50448240aa9971dcdb5
Steven Chen <syc.stevenchen@gmail.com>
2020-05-13 15:47:00 -0400

    fix

--- code/network.py
+++ code/network.py
@@ -3,63 +3,63 @@ import torch.nn.functional as F
 import torch
 
 class GazeEstimationNet(nn.Module):
-	def __init__(self):
-		super(GazeEstimationNet, self).__init__()
-		self.conv1 = nn.Conv2d(3,96, kernel_size=11, stride=4)
-		self.skip_conv1 = nn.Conv2d(96, 256, kernel_size=7, stride=4)
-		self.features1 = nn.Sequential(
-			nn.BatchNorm2d(96),
-			nn.MaxPool2d(kernel_size=3, stride=2),
-			nn.ReLU(True),
-			nn.Conv2d(96, 256, kernel_size=5, stride=2),
-			nn.BatchNorm2d(256),
-			nn.MaxPool2d(kernel_size=3, stride=2),
-			nn.ReLU(True),
-			nn.Conv2d(256, 384, kernel_size=3),
-			nn.ReLU(True)
-		)
+    def __init__(self):
+        super(GazeEstimationNet, self).__init__()
+        self.conv1 = nn.Conv2d(3,96, kernel_size=11, stride=4)
+        self.skip_conv1 = nn.Conv2d(96, 256, kernel_size=7, stride=4)
+        self.features1 = nn.Sequential(
+            nn.BatchNorm2d(96),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.ReLU(True),
+            nn.Conv2d(96, 256, kernel_size=5, stride=2),
+            nn.BatchNorm2d(256),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.ReLU(True),
+            nn.Conv2d(256, 384, kernel_size=3),
+            nn.ReLU(True)
+        )
 
-		self.skip_conv2 = nn.Conv2d(384, 256, kernel_size=1)
-		self.skip_conv3 = nn.Conv2d(512, 256, kernel_size=3, stride=2)
+        self.skip_conv2 = nn.Conv2d(384, 256, kernel_size=1)
+        self.skip_conv3 = nn.Conv2d(512, 256, kernel_size=3, stride=2)
 
-		self.features2 = nn.Sequential(
-			nn.Conv2d(384, 384, kernel_size=3),
-			nn.ReLU(True),
-			nn.Conv2d(384, 256, kernel_size=3),
-			nn.MaxPool2d(kernel_size=3, stride=2),
-			nn.ReLU(True),
+        self.features2 = nn.Sequential(
+            nn.Conv2d(384, 384, kernel_size=3),
+            nn.ReLU(True),
+            nn.Conv2d(384, 256, kernel_size=3),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.ReLU(True),
 
-		)
+        )
 
-		self.regression = nn.Sequential(
-			#nn.Linear(512 *  , 4096), TODO: calculate input features
-			nn.ReLU(True),
-			nn.Linear(4096, 512),
-			nn.ReLU(True),
-			nn.Linear(512, 2)
-		)
+        self.regression = nn.Sequential(
+            #nn.Linear(512 *  , 4096), TODO: calculate input features
+            nn.ReLU(True),
+            nn.Linear(4096, 512),
+            nn.ReLU(True),
+            nn.Linear(512, 2)
+        )
 
 
-	def forward(self,x):
-		x = F.relu(self.conv1(x))
-		x1 = self.skip_conv1(x)
+    def forward(self,x):
+        x = F.relu(self.conv1(x))
+        x1 = self.skip_conv1(x)
 
-		x = self.features1(x)
+        x = self.features1(x)
 
-		x2 = F.relu(self.skip_conv2(x))
+        x2 = F.relu(self.skip_conv2(x))
 
-		x3 = torch.cat((x1,x2),1)
-		x3 = self.skip_conv3(x3)
+        x3 = torch.cat((x1,x2),1)
+        x3 = self.skip_conv3(x3)
 
-		x = self.features2(x)
-		x = torch.cat((x,x3),1)
+        x = self.features2(x)
+        x = torch.cat((x,x3),1)
 
-		x = torch.flatten(x,1)
+        x = torch.flatten(x,1)
 
-		#x = self.regression(x)
-		return x
+        #x = self.regression(x)
+        return x
 
 if __name__ == '__main__':
-	#from torchsummary import summary
-	model = GazeEstimationNet()
-	print(model)
+    #from torchsummary import summary
+    model = GazeEstimationNet()
+    print(model)
--- code/train.py
+++ code/train.py
@@ -11,72 +11,72 @@ import os
 import pandas as pd
 
 class GazeDataset(Dataset):
-	def __init__(self, data_csv_path, transforms = None):
-		self.path = path
-		self.data_csv = pd.read_csv(data_csv_path)
-		self.transforms = transforms
-		self.data = []
+    def __init__(self, data_csv_path, transforms = None):
+        self.path = path
+        self.data_csv = pd.read_csv(data_csv_path)
+        self.transforms = transforms
+        self.data = []
 
-		paths = self.data_csv['path'] + '/spot'+self.data_csv['spot']+'/video'+self.data_csv['video']+self.data_csv['file']
+        paths = self.data_csv['path'] + '/spot'+self.data_csv['spot']+'/video'+self.data_csv['video']+self.data_csv['file']
 
 
 
-	def __len__(self):
-		return len(self.data)
-	def __getitem__(self, idx):
-		path, target = self.data[idx]
-		image = Image.open(path)
-		if self.transforms is not None:
-			sample = self.transform(sample)
-		return sample, target 
+    def __len__(self):
+        return len(self.data)
+    def __getitem__(self, idx):
+        path, target = self.data[idx]
+        image = Image.open(path)
+        if self.transforms is not None:
+            sample = self.transform(sample)
+        return sample, target 
 
 
 class AverageMeter:
-	def __init__(self):
-		self.reset()
-	def reset(self):
-		self.val = 0
-		self.avg = 0
-		self.sum = 0
-		self.count = 0
+    def __init__(self):
+        self.reset()
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
 
-	def update(self, val, n=1):
-		self.val = val
-		self.sum += val * n
-		self.count += n
-		self.avg = self.sum / self.count 
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count 
 
 
 
 def train(train_loader, model, criterion, optimizer, epochs, dev):
 
-	model.train() 
+    model.train() 
 
-	for i, (images, target) in enumerate(train_loader):
-		images = images.to(dev)
-		target = target.to(dev)
+    for i, (images, target) in enumerate(train_loader):
+        images = images.to(dev)
+        target = target.to(dev)
 
-		output = model(images)
-		loss = criterion(output, target)
+        output = model(images)
+        loss = criterion(output, target)
 
-		optimizer.zero_grad()
-		loss.backward()
-		optimizer.step()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
 def validate(val_loader, model, criterion, dev):
-	model.eval()
+    model.eval()
 
-	with torch.no_grad():
-		for i, (images, target) in enumerate(val_loader):
-			images = images.to(dev)
-			target = target.to(dev)
+    with torch.no_grad():
+        for i, (images, target) in enumerate(val_loader):
+            images = images.to(dev)
+            target = target.to(dev)
 
-			output = model(images)
-			loss = criterion(output, target)
+            output = model(images)
+            loss = criterion(output, target)
 
 
 if __name__ == '__main__':
-	pass
+pass
 
 
 
